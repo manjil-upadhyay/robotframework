6d62893c5dbb2ebde25781132c40deda9a6a3827
manjil-upadhyay <63592333+manjil-upadhyay@users.noreply.github.com>
2023-02-10 19:47:28 +0530

    Add files via upload

---
+++ TMPMasterTS.bat
@@ -0,0 +1,16 @@
+TITLE ERP Script Execution
+ECHO ===================================
+ECHO JM END TO END SCRTIP EXCUECUTION
+ECHO ===================================
+ECHO =*******************************************=
+ECHO Please Keep Patience while Script Execution
+ECHO =*******************************************=
+@ECHO
+cd C:\AecorAutomation\EuroSport
+call TC.bat
+call test.bat
+
+ECHO .
+ECHO Have a Nice Day!
+ECHO =******************************************************=
+PAUSE
